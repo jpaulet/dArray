951a870d169e271c2a6f0cf71f02f3f643a50b96
jp aulet <blakdrak@gmail.com>
2019-10-31 15:28:20 +0100

    Solved eslint errors

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from "vue-router";
+import VueRouter from 'vue-router'
 
-//import '@/assets/scss/white-dashboard.scss';
-//import '@/assets/css/nucleo-icons.css';
-//import "@/assets/demo/demo.css";
+// import '@/assets/scss/white-dashboard.scss';
+// import '@/assets/css/nucleo-icons.css';
+// import "@/assets/demo/demo.css";
 
-import GlobalComponents from "./globalComponents";
-import GlobalDirectives from "./globalDirectives";
-import Notify from "@/components/NotificationPlugin";
-import i18n from "./i18n"
-import SideBar from "@/components/SidebarPlugin";
+import GlobalComponents from './globalComponents'
+import GlobalDirectives from './globalDirectives'
+import Notify from '@/components/NotificationPlugin'
+import i18n from './i18n'
+import SideBar from '@/components/SidebarPlugin'
 
-//var $ = window.jQuery = require('jquery')
+// router setup
+import routes from './router'
+
+// var $ = window.jQuery = require('jquery')
 
 window.blockstack = require('blockstack')
 window.axios = require('axios')
 
 Vue.config.productionTip = false
 
-// router setup
-import routes from "./router";
-
 // configure router
 const router = new VueRouter({
   routes, // short for routes: routes
-  linkExactActiveClass: "active"
-});
+  linkExactActiveClass: 'active'
+})
 
-Vue.use(VueRouter);
-Vue.use(GlobalComponents);
-Vue.use(GlobalDirectives);
-Vue.use(SideBar);
-Vue.use(Notify);
+Vue.use(VueRouter)
+Vue.use(GlobalComponents)
+Vue.use(GlobalDirectives)
+Vue.use(SideBar)
+Vue.use(Notify)
 
 new Vue({
   router,
